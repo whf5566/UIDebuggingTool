1b86d1e92b627cf243a349be2d509953ea7619d6
whf5566 <whf5566@gamil.com>
2017-12-08 11:03:52 +0800

    Simulate two finger click status bar events for ios11

--- UIDebuggingTool/ViewController.m
+++ UIDebuggingTool/ViewController.m
@@ -8,6 +8,19 @@
 
 #import "ViewController.h"
 
+@interface OverlayWindow : UIWindow
+- (UIGestureRecognizerState)state;
+@end
+
+@implementation OverlayWindow
+// [[UIDebuggingInformationOverlayInvokeGestureHandler mainHandler] _handleActivationGesture:(UIGestureRecognizer *)]
+// requires a UIGestureRecognizer, as it checks the state of it. We just fake that here.
+- (UIGestureRecognizerState)state {
+    return UIGestureRecognizerStateEnded;
+}
+@end
+
+
 @interface ViewController ()
 
 @end
@@ -21,15 +34,27 @@
 
 - (IBAction)onClickToggleVisibility:(id)sender {
     
-    //  Tap status bar with two fingers to start UIDebuggingInformationOverlay
+    // Tap status bar with two fingers to start UIDebuggingInformationOverlay
+    // 两个手指同时点击状态栏弹出调试框
     
 #if DEBUG
     
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-    id overlayClass = NSClassFromString(@"UIDebuggingInformationOverlay");
-    Class overlay = [overlayClass performSelector:NSSelectorFromString(@"overlay")];
-    [overlay performSelector:NSSelectorFromString(@"toggleVisibility")];
+    if (@available(iOS 11.0, *)) {
+        // Simulate two finger click status bar events
+        // 模拟两个手指点击状态栏的事件
+        id overlayClass = NSClassFromString(@"UIDebuggingInformationOverlay");
+        [overlayClass performSelector:NSSelectorFromString(@"overlay")];
+        id handlerClass = NSClassFromString(@"UIDebuggingInformationOverlayInvokeGestureHandler");
+
+        id handler = [handlerClass performSelector:NSSelectorFromString(@"mainHandler")];
+        [handler performSelector:NSSelectorFromString(@"_handleActivationGesture:") withObject:[[OverlayWindow alloc] init]];
+    } else {
+        id overlayClass = NSClassFromString(@"UIDebuggingInformationOverlay");
+        id overlay = [overlayClass performSelector:NSSelectorFromString(@"overlay")];
+        [overlay performSelector:NSSelectorFromString(@"toggleVisibility")];
+    }
 #pragma clang diagnostic pop
     
 #endif
